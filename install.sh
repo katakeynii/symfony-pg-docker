edf436b76907ea19558e89c5e35013ab0288e13c
Mohamed Camara GUEYE <mohamed.gueye@akilee-by-ines.com>
2019-04-10 17:06:44 +0000

    updating compose file and install echos

--- install.sh
+++ install.sh
@@ -12,25 +12,7 @@ echo "${green}====== GENERATING A NEW SYMFONY APP ================="
 echo "${green}====================================================="
 echo "${green}-"
 echo "${green}-"
-echo "${green}--------------------------------------------------"
-echo "${green}--------------------------------------------------"
-USAGE=$(cat <<-END
-		NB: All options available while creating a rails app 
-		with 'rails new' are available here .
-		And don't forget to give to your user the owner 
-		Ex: chown -R <your_user><your_user> .
 
-		--------------------------------------------------
-		--------------------------------------------------
-
-    NB: Tous les options disponibles avec la commande 
-    'rails new' sont disponibles ici.
-    Et n'oubliez pas de donner à votre utilisateur la propriété et
-    tous les droits nécessaire
-		Ex: chown -R <your_user><your_user> .
-END
-)
-echo "${red} $USAGE"
 echo "${reset}"
 sudo cp .env.default .env
 sudo docker-compose build
